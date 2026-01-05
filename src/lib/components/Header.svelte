d6b69c61a39527ea0b469bbd0b5b2ad1a45388e0
Manuel Grgic <manuel.grgic@weatherbound.com>
2026-01-05 17:32:18 +0100

    final header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
     import { browser } from "$app/environment";
-
-    import NavButtons from "./NavButtons.svelte";
+	import { onMount } from "svelte";
     
     let darkMode = true;
     let altHeader = false;
     
     const changePx = 100;
+    
     const links = [
+        {id: 'home', label: 'Home'},
         {id: 'about', label: 'About me'},
         {id: 'projects', label: 'Project'},
         {id: 'resume', label: 'Resume'},
         {id: 'contact', label: 'Contact me'}
-    ]
+    ];
+
+    const buttons = [
+        {url: 'https://github.com/ManuGr', icon: 'icon-[line-md--github]', label: 'github link'},
+        {url: 'https://www.linkedin.com/in/manuel-grgic/', icon:'icon-[mdi--linkedin]', label: 'linkedin link'}
+    ];
 
     function handleScroll() {
         altHeader = document.documentElement.scrollTop > changePx;
@@ -29,6 +35,8 @@
             : document.documentElement.classList.remove('dark');
     }
 
+    let target = null;
+
     if (browser) {
         if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark');
@@ -38,26 +46,61 @@
             darkMode = false;
         }
     }
+
+    onMount(() => {
+
+        function handleIntersection(entries) {
+            entries.map((entry) => {
+                if(entry.isIntersecting) {
+                    document.querySelector(`#${entry.target.id}-link`)?.classList.add('dark:text-[#ccc]', 'text-[#555]', 'border-b-2', 'border-[#555]', 'dark:border-[#ccc]');
+                } else {
+                    document.querySelector(`#${entry.target.id}-link`)?.classList.remove('dark:text-[#ccc]', 'text-[#555]', 'border-b-2', 'border-[#555]', 'dark:border-[#ccc]');
+                }
+            });
+        }
+
+        const options = {
+            threshold: 0.55
+        }
+
+        const observer = new IntersectionObserver(handleIntersection, options);
+
+        document.querySelectorAll('#home, #about, #projects, #resume, #contact').forEach((el) => {
+            if(el) {
+                observer.observe(el);
+            }
+        });
+    });
 </script>
 
 <svelte:window on:scroll={handleScroll} />
 
 <header class="fixed w-full flex justify-center">
-    <div class="fixed {altHeader ? 'w-2/3 rounded-full mt-4' : 'w-full'} flex items-center justify-between p-4 {darkMode ? 'bg-white/5' : 'bg-black/12'} backdrop-blur-xl transition-all duration-300 ease-in-out">
-        <div>
-            <h1 class="text-xl font-semibold">Manuel Figueiredo</h1>
-        </div>
+    <div class="{altHeader ? 'w-2/3 rounded-full mt-4' : 'w-full'} flex items-center justify-between p-4 {darkMode ? 'bg-white/5' : 'bg-black/12'} backdrop-blur-xl transition-all duration-300 ease-in-out">
         <ul class="flex items-center gap-2">
-            <li>
-                <button class="p-2 flex items-center justify-center rounded-full {darkMode ? 'bg-white/25' : 'bg-black/15'} transition-colors duration-200 ease-in-out" on:click={handleSwitch}>
-                    <span class="material-symbols-outlined">
-                        {darkMode ? 'dark_mode' : 'light_mode'}
-                    </span>
-                </button>
-            </li>
             {#each links as link}
                 <li>
-                    <NavButtons id={link.id} label={link.label} />
+                    <a
+                        id="{link.id}-link"
+                        href="#{link.id}"
+                        class="px-3 py-1.5 dark:hover:text-[#ccc] hover:text-[#555] transition-colors duration-200 w-11/12"
+                    >
+                        {link.label}
+                    </a>
+                </li>
+            {/each}
+        </ul>
+        <ul class="flex items-center gap-2">
+            <li>
+                <button class="p-2 flex items-center justify-center rounded-full {darkMode ? 'bg-white/25' : 'bg-black/15'} text-xl hover:scale-110 transition-all duration-300 ease-in-out" on:click={handleSwitch}>
+                    <span class="icon-[material-symbols--{darkMode ? 'dark' : 'light'}-mode-outline-rounded]">toggle mode</span>
+                </button>
+            </li>
+            {#each buttons as btn}
+                <li>
+                    <a href={btn.url} class="p-2 flex items-center justify-center rounded-full {darkMode ? 'bg-white/25' : 'bg-black/15'} text-xl hover:scale-110 transition-all duration-300 ease-in-out">
+                        <span class={btn.icon}>{btn.label}</span>
+                    </a>
                 </li>
             {/each}
         </ul>
