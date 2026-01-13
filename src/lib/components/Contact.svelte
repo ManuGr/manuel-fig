277c8dc9c9707063ed5284a9be668e1d10fa47d4
Manuel Figueiredo <manel.gf.95@gmail.com>
2026-01-13 12:28:39 +0100

    initial contact form

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -3,5 +3,15 @@
 </script>
 
 <div id="contact" class="w-full h-dvh flex items-center justify-center">
-    <h1>Contact</h1>
+    <div class="w-full flex items-center justify-center bg-black/10 dark:bg-white/2">
+        <div class="w-11/12 lg:w-3/5 flex flex-col gap-6 p-10">
+            <h1 class="text-xl font-semibold">Contact me</h1>
+            <form class="w-full flex flex-col items-end justify-around gap-2">
+                <input type="email" class="w-full border rounded-lg px-4 py-2" placeholder="E-mail address">
+                <input type="text" class="w-full border rounded-lg px-4 py-2" placeholder="Subject">
+                <input type="textarea" class="w-full border rounded-lg min-h-50 px-4 py-2" placeholder="Message">
+                <button class="border-2 px-4 py-1 rounded-lg hover:scale-105 transition-transform duration-200">Send</button>
+            </form>
+        </div>
+    </div>
 </div>
